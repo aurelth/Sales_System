fe00dc189d23ed778e00ad7dd498db9a85157de1
aurelth <aurelth@icloud.com>
2020-07-08 22:55:18 -0300

    Validations

--- SalesWebSystemMvc/Controllers/SellersController.cs
+++ SalesWebSystemMvc/Controllers/SellersController.cs
@@ -43,6 +43,13 @@ namespace SalesWebSystemMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
             _sellerService.InsertDB(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -110,7 +117,14 @@ namespace SalesWebSystemMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if(id != seller.Id)
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
+            if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not mismatch" });
             }
